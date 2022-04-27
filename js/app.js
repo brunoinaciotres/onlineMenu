d518832fa8b6e84b0cd3cb18919f449b1b16a2be
brunoinaciotres <btres76@gmail.com>
2022-04-27 13:35:44 -0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,65 @@
+import {bebidas} from './drinks.js'
+
+
+
+const ulCervejas = document.querySelector('#cervejas')
+const ulChopps = document.querySelector('#chopps')
+
+
+// body.appendChild(article)
+
+for (let i = 0; i < bebidas[1].length; i++) {
+    createLiCervejas(i)
+}
+
+for (let i = 0; i < bebidas[2].length; i++) {
+    createLiChopps(i)
+}
+
+
+
+function createLiCervejas (i){
+    
+    const h4 = document.createElement('h4')
+    h4.classList.add('title-description-container', 'h4')
+    const span = document.createElement('span')
+    const titleDescriptionContainer = document.createElement('div')
+    titleDescriptionContainer.classList.add('title-description-container', 'center')
+    const price = document.createElement('div')
+    price.classList.add('price', 'center')
+    const li = document.createElement('li')
+    li.classList.add('product-item')
+    titleDescriptionContainer.appendChild(h4)
+    titleDescriptionContainer.appendChild(span)
+    li.appendChild(titleDescriptionContainer)
+    li.appendChild(price)
+
+    h4.innerText = bebidas[1][i].title
+    span.innerText = bebidas[1][i].desc
+    price.innerText = bebidas[1][i].price
+
+    ulCervejas.appendChild(li)
+}
+
+function createLiChopps(i) {
+    const li = document.createElement('li')
+    li.classList.add('product-item', 'vertical')
+    const h4 = document.createElement('h4')
+    const span = document.createElement('span')
+    const span2 = document.createElement('span')
+
+    li.appendChild(h4)
+    li.appendChild(span)
+    li.appendChild(span2)
+
+    h4.innerText = bebidas[2][i].title
+    span.innerText = bebidas[2][i].desc
+    span2.innerText = bebidas[2][i].price
+    
+    ulChopps.appendChild(li)
+}
+
+
+
+
+
---
+++ js/drinks.js
@@ -0,0 +1,200 @@
+
+// const drinks = {
+//     title: 'Bebidas',
+//     src: './assets/icons/beer-icon.png',
+//     cervejas: {
+//         title: 'Cervejas',
+//         products: {
+//             chopps: {
+
+//                 choppCaneca: {
+//                     title: 'Caneca',
+//                     desc: '415ml',
+//                     price: 'R$12,90'
+//                 },
+//                 choppCaldereta: {
+//                     title: 'Caldereta',
+//                     desc: '350ml',
+//                     price: 'R$9,90'
+//                 },
+//                 choppCaldereta: {
+//                     title: 'Baby',
+//                     desc: '200ml',
+//                     price: 'R$7,20'
+//                 }
+//             },
+//             bottles: {
+
+//                 bohemia: {
+//                     title: 'Bohemia',
+//                     desc: '600ml',
+//                     price: 'R$10,90'
+//                 },
+//                 becks: {
+//                     title: 'Beck\'s',
+//                     desc: '600ml',
+//                     price: 'R$16,90'
+//                 },
+//                 original: {
+//                     title: 'Original',
+//                     desc: '600ml',
+//                     price: 'R$12,90'
+//                 },
+//                 stella: {
+//                     title: 'Stella Artois',
+//                     desc: '600ml',
+//                     price: 'R$14,90'
+//                 },
+//                 budweiser: {
+//                     title: 'Budweiser',
+//                     desc: '600ml',
+//                     price: 'R$12,50'
+//                 },
+//                 longNeck: {
+//                     title: 'Long Neck',
+//                     desc: '600ml',
+//                     price: 'R$9,90'
+//                 }
+//             }
+//         }
+       
+//     },
+//     bebidas: {
+//         refri: {
+//             title: 'Refrigerante',
+//             desc: '350ml',
+//             price: 'R$5,90'
+//         },
+//         águaSG: {
+//             title: 'Água',
+//             desc: 'Sem Gás',
+//             price: 'R$4,00'
+//         },
+//         águaCG: {
+//             title: 'Água',
+//             desc: 'Com Gás',
+//             price: 'R$4,50'
+//         },
+//         sucoNatural: {
+//             title: 'Suco Natural',
+//             desc: 'Verificar frutas disponíveis',
+//             price: 'R$8,90'
+//         },
+//         limonadaSuica: {
+//             title: 'Limonada Suíça',
+//             desc: 'Cremosa',
+//             price: 'R$9,50'
+//         },
+//         sucoDelValle: {
+//             title: 'Suco Del Valle',
+//             desc: 'Verificar sabores disponíveis',
+//             price: 'R$8,50'
+//         },
+//         h2oh: {
+//             title: 'H20H',
+//             desc: 'Limão ou Limoneto',
+//             price: 'R$8,80'
+//         },
+//         schweppes: {
+//             title: 'Schweppes',
+//             desc: 'Citrus ou Tônica',
+//             price: 'R$8,50'
+//         },
+//         redBull: {
+//             title: 'Red Bull',
+//             desc: 'Verificar sabores disponíveis',
+//             price: 'R$15,90'
+//         },
+//     }
+// }
+const bebidas = [
+
+    
+        [
+            {
+                title: 'Refrigerante',
+                price: 'R$5,90'
+            }
+        ],
+        [
+            
+            {
+                title: 'Bohemia',
+                desc: '600ml',
+                price: 'R$10,90'
+            },
+            {
+                title: 'Beck\'s',
+                desc: '600ml',
+                price: 'R$16,90'
+            },
+            {
+                title: 'Original',
+                desc: '600ml',
+                price: 'R$12,90'
+            },
+            {
+                title: 'Stella Artois',
+                desc: '600ml',
+                price: 'R$14,90'
+            },
+            {
+                title: 'Budweiser',
+                desc: '600ml',
+                price: 'R$12,50'
+            },
+            {
+                title: 'Long Neck',
+                desc: '600ml',
+                price: 'R$9,90'
+            },
+
+        ],
+        [
+            
+            {
+                title: 'Caneca',
+                desc: '415ml',
+                price: 'R$12,90'
+            },
+            {
+                title: 'Caldereta',
+                desc: '350ml',
+                price: 'R$9,90'
+            },
+            {
+                title: 'Baby',
+                desc: '200ml',
+                price: 'R$7,20'
+            }
+        ],
+        [
+            {
+                title:'Dom Bré',
+                price: 'R$9,50'
+            }
+
+        ],
+        [
+            {
+                title: 'Sex On The Beach',
+                price: 'R$16,80'
+            },
+            {
+                title: 'Mojito',
+                price: 'R$15,80'
+            },
+            {
+                title: 'Caipirinha',
+                price: 'R$14,90'
+            }
+        ],
+    
+]
+
+
+export {
+    bebidas,  
+    
+    
+} 
